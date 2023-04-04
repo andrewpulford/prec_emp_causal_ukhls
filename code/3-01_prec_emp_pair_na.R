beb94f94e68ee8af0c4a586a478c9d634a6b48f7
Andrew Pulford <andrew.pulford@phs.scot>
2023-04-04 19:34:44 +0100

    added code to create analytic sample and exposure vars

--- code/3-01_prec_emp_pair_na.R
+++ code/3-01_prec_emp_pair_na.R
@@ -1,7 +1,7 @@
 ################################################################################
 
 # Precarious employment and health - Understanding Society
-# 3-01 - Missing data 
+# 3-01 - Create analytic sample 
 # Andrew Pulford
 
 # Data source:
@@ -12,6 +12,7 @@
 
 #### What this script does:
 # (a) Checks item missingness across survey waves 
+# (b) create final complete case df 
 
 
 ################################################################################
@@ -79,4 +80,69 @@ pair_cc_eligible_na <- pair_cc_eligible_na %>%
                    .fns = ~sum(.x))) %>% 
   pivot_longer(cols=1:120, names_to = "variable", values_to = "n_NA") %>% 
   mutate(pc_NA = n_NA/n_row*100)
+
+
+################################################################################
+#####                     create final complete case df                    #####
+################################################################################
+
+# Exclude:
+# missing employment status at t1
+# missing outcomes - SRH, GHQ-12, SF-12 PCS, SF-12 MCS 
+
+# create flag for missing employment status at t1
+pair_cc_analytic <- pair_cc_eligible  %>% 
+  mutate(exposure_na = ifelse(jbstat_t1%in%missing_vector,1,0)) 
+
+# number of cases excluded due to missing exposure at t1
+pair_cc_analytic_na <- pair_cc_analytic %>% 
+  summarise(n = sum(exposure_na)) %>% 
+  mutate(na_lab = "exposure missing")
+
+pair_cc_analytic <- pair_cc_analytic %>%
+# exclude missing exposure cases
+  filter(exposure_na==0) %>% 
+# create flag for missing outcome at t1
+  mutate(outcome_na = ifelse(srh_dv_t1%in%missing_vector,1,
+                      ifelse(ghq_case4_t1%in%missing_vector,1,
+                      ifelse(sf12mcs_dv_t1%in%missing_vector,1,
+                      ifelse(sf12mcs_dv_t1%in%missing_vector,1,0))))) 
+
+# number of cases excluded due to missing outcome at t1
+temp <- pair_cc_analytic %>% 
+  summarise(n = sum(outcome_na)) %>% 
+  mutate(na_lab = "outcome missing")
+
+# bind together with exposure na
+pair_cc_analytic_na <- pair_cc_analytic_na %>% 
+  bind_rows(temp)
+
+# exclude missing outcome cases
+pair_cc_analytic <- pair_cc_analytic %>% 
+  filter(outcome_na==0)
+
+################################################################################
+#####                       create exposure variables                      #####
+################################################################################
+
+pair_cc_analytic <- pair_cc_analytic %>%
+  # unemployed at t1
+  mutate(exposure1 = ifelse(jbstat_t1%in%c("unemployed","Unemployed"),
+                            "exposed (unemployed at t1)","unexposed")) %>% 
+  # job loss between t0 and t1
+  mutate(exposure2 = ifelse(jbstat_t1 %in% c("unemployed","unemployed"), 
+                            "exposed (job loss between t0 and t1",
+                           ifelse(nunmpsp_dv_t1>0,
+                                  "exposed (job loss between t0 and t1",
+                                  "unexposed"))) 
+
+################################################################################
+#####                               save df's                              #####
+################################################################################
+
+## paired analytic complete case df
+write_rds(pair_cc_analytic, "./working_data/pair_cc_analytic.rds")
+
+## exclusions
+write_rds(pair_cc_analytic_na, "./working_data/pair_cc_analytic_na.rds")
 
