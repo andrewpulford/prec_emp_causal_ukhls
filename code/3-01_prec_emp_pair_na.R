ccefb730bc43a8d6585c5ea2e3cdb696d85f8f62
Andrew Pulford <andrew.pulford@phs.scot>
2023-07-14 14:40:21 +0100

    omit all incomplete cases pror to complete case analysis

--- code/3-01_prec_emp_pair_na.R
+++ code/3-01_prec_emp_pair_na.R
@@ -26,35 +26,61 @@ rm(list=ls())
 ################################################################################
 
 library(tidyverse) # all kinds of stuff 
+library(naniar) # for missing values
 
 ################################################################################
 #####                         load and prepare data                        #####
 ################################################################################
 
+
+####read in variable vectors ---------------------------------------------------
+source("./look_ups/variable_vectors.r")
+
+## extra vars for creating exposure vars
+extra_vars <- c("jbstat_t1", "nunmpsp_dv_t1")
+
 #### load in variable spine and create vector ----------------------------------
-vars <- readRDS("./variables/vars_master.rds")
-  
-analytic_vars <- vars %>% 
-  filter(type %in% c("exp","cov")) %>% 
-  mutate(variable_t0 = paste0(variable,"_t0"),
-         variable_t1 = paste0(variable,"_t1"))
-
-temp1 <- analytic_vars$variable_t0
-temp2 <- analytic_vars$variable_t1
-
-analytic_vars_vec <- c(temp1,temp2)
-rm(temp1,temp2)
+#vars <- readRDS("./variables/vars_master.rds")
+#  
+#analytic_vars <- vars %>% 
+#  filter(type %in% c("exp","cov")) %>% 
+#  mutate(variable_t0 = paste0(variable,"_t0"),
+#         variable_t1 = paste0(variable,"_t1"))
+#
+#temp1 <- analytic_vars$variable_t0
+#temp2 <- analytic_vars$variable_t1
+#
+#analytic_vars_vec <- c(temp1,temp2)
+#rm(temp1,temp2)
 
 ####load eligible cases --------------------------------------------------------
-pair_cc_eligible <- readRDS("./working_data/pair_cc_eligible.rds")# %>% 
-#  dplyr::select(analytic_vars_vec)
+pair_cc_eligible <- readRDS("./working_data/pair_eligible.rds") %>% 
+  dplyr::select(all_of(c(id_wt_vector, 
+                         cov_vector, cov_vector2, 
+                         outcome_vector2,
+                         extra_vars))) %>% 
+  dplyr::select(-c(psu, strata, wt_name, wt_value))
 
+################################################################################
+#####                       create exposure variables                      #####
+################################################################################
+
+pair_cc_eligible <- pair_cc_eligible %>%
+  # unemployed at t1
+  mutate(exposure1 = ifelse(jbstat_t1%in%c("unemployed","Unemployed"),
+                            "exposed (unemployed at t1)","unexposed")) %>% 
+  # job loss between t0 and t1
+  mutate(exposure2 = ifelse(jbstat_t1 %in% c("unemployed","unemployed"), 
+                            "exposed (job loss between t0 and t1",
+                            ifelse(nunmpsp_dv_t1>0,
+                                   "exposed (job loss between t0 and t1",
+                                   "unexposed"))) 
 
 ################################################################################
 #####                             create NAs df                            #####
 ################################################################################
 
-missing_vector <- c("missing", 
+missing_vector <- c("missing", "Missing",
                     "inapplicable", "proxy",
                     "refusal", 
                     "Only available for IEMB", 
@@ -78,70 +104,77 @@ n_row <- nrow(pair_cc_eligible_na)
 pair_cc_eligible_na <- pair_cc_eligible_na %>% 
   summarise(across(.cols=everything(),
                    .fns = ~sum(.x))) %>% 
-  pivot_longer(cols=1:120, names_to = "variable", values_to = "n_NA") %>% 
-  mutate(pc_NA = n_NA/n_row*100)
+  pivot_longer(cols=everything(), names_to = "variable", values_to = "n_NA") %>% 
+  mutate(pc_NA = n_NA/n_row*100) 
+
 
 ################################################################################
 #####                     create final complete case df                    #####
 ################################################################################
 
+### recode missing categories as NA
+start_time <- Sys.time()
+pair_cc_analytic <- pair_cc_eligible  %>% 
+  replace_with_na_all(condition = ~.x =="missing")
+end_time <- Sys.time()
+end_time - start_time
+
+# check
+sapply(pair_cc_analytic, function(x) sum(is.na(x)))
+
+### remove any incomplete cases for CC analysis
+pair_cc_analytic <- pair_cc_analytic %>% 
+  na.omit()
+
+#### VVV retain for now - may need for MI VVV ####
 # Exclude:
 # missing employment status at t1
 # missing outcomes - SRH, GHQ-12, SF-12 PCS, SF-12 MCS 
 
 # create flag for missing employment status at t1
-pair_cc_analytic <- pair_cc_eligible  %>% 
-  mutate(exposure_na = ifelse(jbstat_t1%in%missing_vector,1,0)) 
+#pair_cc_analytic <- pair_cc_eligible  %>% 
+#  mutate(exposure_na = ifelse(jbstat_t1%in%missing_vector,1,0)) 
 
 # number of cases excluded due to missing exposure at t1
-pair_cc_analytic_na <- pair_cc_analytic %>% 
-  summarise(n = sum(exposure_na)) %>% 
-  mutate(na_lab = "exposure missing")
+#pair_cc_analytic_na <- pair_cc_analytic %>% 
+#  summarise(n = sum(exposure_na)) %>% 
+#  mutate(na_lab = "exposure missing")
 
-pair_cc_analytic <- pair_cc_analytic %>%
+#pair_cc_analytic <- pair_cc_analytic %>%
 # exclude missing exposure cases
-  filter(exposure_na==0) %>% 
+#  filter(exposure_na==0) %>% 
 # create flag for missing outcome at t1
-  mutate(outcome_na = ifelse(srh_dv_t1%in%missing_vector,1,
-                      ifelse(ghq_case4_t1%in%missing_vector,1,
-                      ifelse(sf12mcs_dv_t1%in%missing_vector,1,
-                      ifelse(sf12mcs_dv_t1%in%missing_vector,1,0))))) 
+#  mutate(outcome_na = ifelse(srh_dv_t1%in%missing_vector,1,
+#                      ifelse(ghq_case4_t1%in%missing_vector,1,
+#                      ifelse(sf12mcs_dv_t1%in%missing_vector,1,
+#                      ifelse(sf12mcs_dv_t1%in%missing_vector,1,0))))) 
 
 # number of cases excluded due to missing outcome at t1
-temp <- pair_cc_analytic %>% 
-  summarise(n = sum(outcome_na)) %>% 
-  mutate(na_lab = "outcome missing")
+#temp <- pair_cc_analytic %>% 
+#  summarise(n = sum(outcome_na)) %>% 
+#  mutate(na_lab = "outcome missing")
 
 # bind together with exposure na
-pair_cc_analytic_na <- pair_cc_analytic_na %>% 
-  bind_rows(temp)
+#pair_cc_analytic_na <- pair_cc_analytic_na %>% 
+#  bind_rows(temp)
 
 # exclude missing outcome cases
-pair_cc_analytic <- pair_cc_analytic %>% 
-  filter(outcome_na==0)
+#pair_cc_analytic <- pair_cc_analytic %>% 
+#  filter(outcome_na==0)
 
-################################################################################
-#####                       create exposure variables                      #####
-################################################################################
 
-pair_cc_analytic <- pair_cc_analytic %>%
-  # unemployed at t1
-  mutate(exposure1 = ifelse(jbstat_t1%in%c("unemployed","Unemployed"),
-                            "exposed (unemployed at t1)","unexposed")) %>% 
-  # job loss between t0 and t1
-  mutate(exposure2 = ifelse(jbstat_t1 %in% c("unemployed","unemployed"), 
-                            "exposed (job loss between t0 and t1",
-                           ifelse(nunmpsp_dv_t1>0,
-                                  "exposed (job loss between t0 and t1",
-                                  "unexposed"))) 
 
 ################################################################################
 #####                               save df's                              #####
 ################################################################################
 
+## paired eligible complete case df
+write_rds(pair_cc_eligible, "./working_data/pair_eligible.rds")
+
+
 ## paired analytic complete case df
 write_rds(pair_cc_analytic, "./working_data/pair_cc_analytic.rds")
 
 ## exclusions
-write_rds(pair_cc_analytic_na, "./working_data/pair_cc_analytic_na.rds")
+write_rds(pair_cc_eligible_na, "./working_data/pair_cc_eligible_na.rds")
 
