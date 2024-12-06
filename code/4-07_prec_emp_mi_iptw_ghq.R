1ab5791c2f13adbe11920a25e61bb1da59b47361
Andrew Pulford <andrew.pulford@phs.scot>
2024-12-06 16:09:48 +0000

    added scrapbook

--- code/4-07_prec_emp_mi_iptw_ghq.R
+++ code/4-07_prec_emp_mi_iptw_ghq.R
@@ -114,3 +114,57 @@ weightit_pooled_ghq_df <- data.frame(summary(weightit_pooled_ghq, conf.int = TRU
 
 
 write.csv(weightit_pooled_ghq_df, "./working_data/mi/weightit_pooled_ghq_df.csv")
+
+################################################################################
+##### scrapbook
+################################################################################
+
+
+## fit model to the imputations and pool the results:
+test <- with(data = weightit_df, 
+                          exp = glmmTMB(ghq_case4_t1 ~
+                                          # exposure
+                                          exposure1 +
+                                          # t0 outcome measure
+#                                          ghq_case4_t0 +
+                                          # t0 covariates
+ #                                         sex_dv_t0 +
+  #                                        age_dv_t0 +
+   #                                       non_white_t0 +
+    #                                      marital_status_t0 +
+     #                                     degree_bin_t0 +
+      #                                    gor_dv_t0 +
+       #                                   sic2007_section_lab_t0 +
+        #                                  soc2000_major_group_title_t0 +
+         #                                 jbft_dv_t0 +
+          #                                small_firm_t0 +
+           #                               emp_contract_t0 +
+            #                              broken_emp_t0 +
+             #                             j2has_dv_t0 +
+              #                            rel_pov_t0 +
+               #                           health_t0 +
+                                          # t1 covariates
+                #                          age_dv_t1 +
+                 #                         marital_status_t1 +
+                  #                        health_t1 +
+                                          # interaction terms
+                   #                       sex_dv_t0*age_dv_t0 +
+                    #                      sex_dv_t0*rel_pov_t0 +
+                     #                     age_dv_t0*rel_pov_t0 +
+                                          (1|pidp), 
+                                        family=binomial(link="logit")))
+
+test_pooled <- pool(test)
+
+test_pooled_df <- data.frame(summary(test_pooled, conf.int = TRUE)) %>% 
+  rename(lci = X2.5..,
+         uci = X97.5..)  %>% 
+  mutate(term = str_remove(term, "exposure1"),
+         outcome = "GHQ-12 caseness",
+         est_type = "coefficient",
+         p.value = ifelse(p.value<0.001,"<0.001",
+                          ifelse(p.value<0.01,"<0.01",
+                                 ifelse(p.value<0.05,"<0.05",       
+                                        p.value)))) %>% 
+  dplyr::select("outcome", "term",	"est_type",	"estimate",	"std.error",	"p.value",	"lci",	"uci")
+
