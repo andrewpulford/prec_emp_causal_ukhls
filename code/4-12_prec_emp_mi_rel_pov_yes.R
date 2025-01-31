03d6913f4ee8c13cedadfb924e5605a79e137f02
Andrew Pulford <andrew.pulford@phs.scot>
2025-01-31 16:34:23 +0000

    fixed model family

--- code/4-12_prec_emp_mi_rel_pov_yes.R
+++ code/4-12_prec_emp_mi_rel_pov_yes.R
@@ -366,7 +366,8 @@ iptw_dr_srh <- function(data){
                  sex_dv_t0*age_dv_t0 +
                  #                                    sex_dv_t0*rel_pov_t0 +
                  #                                    age_dv_t0*rel_pov_t0 +
-                 (1|pidp)))
+                 (1|pidp),
+               family=binomial(link="logit")))
 }
 
 ### ghq-12
@@ -397,7 +398,8 @@ iptw_dr_ghq <- function(data){
                  sex_dv_t0*age_dv_t0 +
                  #                                    sex_dv_t0*rel_pov_t0 +
                  #                                    age_dv_t0*rel_pov_t0 +
-                 (1|pidp)))
+                 (1|pidp),
+               family=binomial(link="logit")))
 }
 
 #### females -------------------------------------------------------------------
