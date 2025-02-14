faeabd18f95f10c351095ef81661f76836db9c18
Andrew Pulford <andrew.pulford@phs.scot>
2025-02-14 13:24:17 +0000

    fixing sf-12 in tables

--- code/5-03_prec_emp_mi_iptw_desc_exp2.R
+++ code/5-03_prec_emp_mi_iptw_desc_exp2.R
@@ -79,14 +79,14 @@ bal.plot(weightit_df, which.imp = 1,
          which = "both")
 
 ## create love plot to visualise balance between unmatched and matched data across MIs
-tiff("./output/mi/mi_descriptives/iptw_love_plot.tiff", width = 980)
+tiff("./output/mi/mi_descriptives/iptw_love_plot_exp2.tiff", width = 980)
 love.plot(weightit_df, thresholds = 0.1, stats = "m",
           drop.distance = TRUE, binary = "std", continuous = "std",
           sample.names = c("Unweighted","Inverse probability of treatment weighted")) 
 # var.names() - to clean up names
 dev.off()
 
-pdf("./output/mi/mi_descriptives/iptw_love_plot.pdf", width = 980, height = 980)
+pdf("./output/mi/mi_descriptives/iptw_love_plot_exp2.pdf", width = 980, height = 980)
 love.plot(weightit_df, thresholds = 0.1, stats = "m",
           drop.distance = TRUE, binary = "std", continuous = "std",
           sample.names = c("Unweighted","Inverse probability of treatment weighted")) 
@@ -101,34 +101,34 @@ dev.off()
 
 ### create complete df of all imputations
 weightit_df_complete <- complete(weightit_df, "long") %>% 
-# rename weights as it throws an error otherwise
-    rename("ps_weights" = "weights") %>% 
+  # rename weights as it throws an error otherwise
+  rename("ps_weights" = "weights") %>% 
   mutate(dep_child_bin_t0 = ifelse(dep_child_bin_t0==1,"Yes","No"))
 
 ### create IPTW weighted df
 svy_weightit_df_complete <- svydesign(ids = ~1,
-                      data = weightit_df_complete,
-                      weights = ~ps_weights)
+                                      data = weightit_df_complete,
+                                      weights = ~ps_weights)
 
 ### create table one
-table_one <- svyCreateTableOne(vars = cov_vector,
-  data=svy_weightit_df_complete, 
-#  factorVars=catVars_short_vec,
-  strata = "exposure1", 
-  test =FALSE)
+table_one <- svyCreateTableOne(vars = c(cov_vector),
+                               data=svy_weightit_df_complete, 
+                               #  factorVars=catVars_short_vec,
+                               strata = "exposure2", 
+                               test =FALSE)
 
 
 ## non-normal numeric vector for table one
 # temp df with only numeric vars
 temp <- weightit_df_complete %>% dplyr::select(c(age_dv_t0,
-                                             sf12pcs_dv_t0,
-                                             sf12mcs_dv_t0))
+                                                 sf12pcs_dv_t0,
+                                                 sf12mcs_dv_t0))
 # vector for numeric vars
 nonnorm_vec <- colnames(temp)
 
 ## printed version for saving
 table_one_sav <- print(table_one, showAllLevels = TRUE, smd = FALSE,
-                       nonnormal = nonnorm_vec,
+                       nonnormal = "age_dv_t0",
                        factorVars = c(catVars_vec),
                        formatOptions = list(big.mark = ",",
                                             scientific = FALSE))
@@ -143,21 +143,26 @@ write.csv(table_one_sav, "./output/mi/sensitivity_analyses/exposure2/weighted_de
 #####                    T0 and T1 outcome descriptives                    #####
 ################################################################################
 
+#### not using
+nonnorm_vec2 <- c("sf12pcs_dv_t0","sf12pcs_dv_t1",
+                  "sf12mcs_dv_t0","sf12mcs_dv_t1")
+
+## create table
 outcomes_desc <- svyCreateTableOne(vars = c("sf12pcs_dv_t0","sf12pcs_dv_t1",
                                             "sf12mcs_dv_t0","sf12mcs_dv_t1",
                                             "srh_bin_t0","srh_bin_t1",
                                             "ghq_case4_t0", "ghq_case4_t1"),
-                                               data=svy_weightit_df_complete, 
-                                               #  factorVars=catVars_short_vec,
-                                               strata = "exposure1", 
-                                               test =FALSE)
+                                   data=svy_weightit_df_complete, 
+                                   #  factorVars=catVars_short_vec,
+                                   strata = "exposure2", 
+                                   test =FALSE)
 
 ## printed version for saving
 outcomes_desc_sav <- print(outcomes_desc, showAllLevels = FALSE, smd = FALSE,
- #                      nonnormal = nonnorm_vec,
-                       factorVars = c(catVars_vec),
-                       formatOptions = list(big.mark = ",",
-                                            scientific = FALSE))
+                           #                           nonnormal = nonnorm_vec2,
+                           #                           factorVars = c(catVars_vec),
+                           formatOptions = list(big.mark = ",",
+                                                scientific = FALSE))
 
 write.csv(outcomes_desc_sav, "./output/mi/sensitivity_analyses/exposure2/weighted_descriptives/outcomes_desc_exp2.csv")
 
@@ -185,8 +190,8 @@ table_one_sav$unexposed_temp2 <- sub("^[^ ]+.","",table_one_sav$unexposed)
 ## divide number of cases by number of imputations (n=25)
 table_one_sav <- table_one_sav %>% 
   mutate(unexposed_temp = ifelse(X %in% c("age_dv_t0 (median [IQR])",
-                                          "sf12mcs_dv_t0 (median [IQR])",
-                                          "sf12pcs_dv_t0 (median [IQR])"),unexposed_temp,
+                                          "sf12mcs_dv_t0 (mean (SD))",
+                                          "sf12pcs_dv_t0 (mean (SD))"),unexposed_temp,
                                  unexposed_temp/25))
 
 ## recreate unexposed var
@@ -195,10 +200,10 @@ table_one_sav$unexposed <- paste(table_one_sav$unexposed_temp,table_one_sav$unex
 #### exposed -------------------------------------------------------------------
 
 ## trim whitespace at both ends of strings
-table_one_sav$exposed..employed.at.t1. <- trimws(table_one_sav$exposed..employed.at.t1.)
+table_one_sav$exposed..no.job.loss.between.t0.and.t1 <- trimws(table_one_sav$exposed..no.job.loss.between.t0.and.t1)
 
 ### extract first part of string
-table_one_sav$exposed_temp <- sub("\\ .*","",table_one_sav$exposed..employed.at.t1.)
+table_one_sav$exposed_temp <- sub("\\ .*","",table_one_sav$exposed..no.job.loss.between.t0.and.t1)
 ## remove commas to allow conversion to numeric
 table_one_sav$exposed_temp <- sub(",","",table_one_sav$exposed_temp)
 table_one_sav$exposed_temp <- sub(",","",table_one_sav$exposed_temp)
@@ -206,19 +211,19 @@ table_one_sav$exposed_temp <- sub(",","",table_one_sav$exposed_temp)
 table_one_sav$exposed_temp <- as.numeric(table_one_sav$exposed_temp)
 
 ## extract second part of string
-table_one_sav$exposed_temp2 <- sub("^[^ ]+.","",table_one_sav$exposed..employed.at.t1)
+table_one_sav$exposed_temp2 <- sub("^[^ ]+.","",table_one_sav$exposed..no.job.loss.between.t0.and.t1)
 
 ## divide number of cases by number of imputations (n=25)
 table_one_sav <-  table_one_sav %>% 
   mutate(exposed_temp = ifelse(X %in% c("age_dv_t0 (median [IQR])",
-                                          "sf12mcs_dv_t0 (median [IQR])",
-                                          "sf12pcs_dv_t0 (median [IQR])"),exposed_temp,
-                                 exposed_temp/25))
+                                        "sf12mcs_dv_t0 (mean (SD))",
+                                        "sf12pcs_dv_t0 (mean (SD))"),exposed_temp,
+                               exposed_temp/25))
 
 ## recreate exposed var
-table_one_sav$exposed..employed.at.t1. <- paste(table_one_sav$exposed_temp,table_one_sav$exposed_temp2)
+table_one_sav$exposed..no.job.loss.between.t0.and.t1 <- paste(table_one_sav$exposed_temp,table_one_sav$exposed_temp2)
 
 ## keep only required cols
-table_one_sav  <-  table_one_sav %>% dplyr::select(c("X", "level", "unexposed", "exposed..employed.at.t1."))
+table_one_sav  <-  table_one_sav %>% dplyr::select(c("X", "level", "unexposed", "exposed..no.job.loss.between.t0.and.t1"))
 
-write.csv(table_one_sav, "./output/mi/sensitivity_analyses/exposure2/weighted_descriptives/table_one_exp2_PAPER.csv")
+write.csv(table_one_sav, "./output/mi/sensitivity_analyses/exposure2/weighted_descriptives/table_one_PAPER.csv")
