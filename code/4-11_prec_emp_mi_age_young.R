e8022d69fa1c072e5f02a28c47bc8310363bece8
Andrew Pulford <andrew.pulford@phs.scot>
2025-02-14 13:22:06 +0000

    fixing sf-12 interaction terms issue

--- code/4-11_prec_emp_mi_age_young.R
+++ code/4-11_prec_emp_mi_age_young.R
@@ -60,6 +60,9 @@ df_y_str <- df_y %>%
   dplyr::group_by(Var1) %>%  
   tidyr::spread(key = Var2, value = Freq)
 
+# check
+sapply(df_y, function(x) sum(is.na(x)))
+
 ################################################################################
 #####                  Younger multiple imputation model                   #####
 ################################################################################
@@ -100,15 +103,15 @@ df_y_str$method[df_y_str$Var1=="marital_status_t1"] <- "polyreg" #polyreg if inc
 df_y_str$method[df_y_str$Var1=="health_t1"] <- "logreg"
 df_y_str$method[df_y_str$Var1=="exp1_bin"] <- ""
 df_y_str$method[df_y_str$Var1=="sex_pcs"] <- "~I(sex_dv_t0*(sf12pcs_dv_t1-mean(sf12pcs_dv_t1)))"
-df_y_str$method[df_y_str$Var1=="sex_mcs"] <- "~I(sex_dv_t0*(sf12mcs_dv_t1-mean(sf12mcs_dv_t1)))"
+df_y_str$method[df_y_str$Var1=="sex_mcs"] <- "~Isf12pcs_dv_t1-mean(sf12mcs_dv_t1, na.rm = TRUE)))"
 df_y_str$method[df_y_str$Var1=="sex_srh"] <- "~I(sex_dv_t0*srh_bin2)"
 df_y_str$method[df_y_str$Var1=="sex_ghq"] <- "~I(sex_dv_t0*ghq_bin)"
-df_y_str$method[df_y_str$Var1=="age_pcs"] <- "~I(age_dv_t0*(sf12pcs_dv_t1-mean(sf12pcs_dv_t1)))"
-df_y_str$method[df_y_str$Var1=="age_mcs"] <- "~I(age_dv_t0*(sf12mcs_dv_t1-mean(sf12mcs_dv_t1)))"
-df_y_str$method[df_y_str$Var1=="age_srh"] <- "~I(age_dv_t0*srh_bin2)"
-df_y_str$method[df_y_str$Var1=="age_ghq"] <- "~I(age_dv_t0*ghq_bin)"
-df_y_str$method[df_y_str$Var1=="rel_pov_pcs"] <- "~I(rel_pov_t0*(sf12pcs_dv_t1-mean(sf12pcs_dv_t1)))"
-df_y_str$method[df_y_str$Var1=="rel_pov_mcs"] <- "~I(rel_pov_t0*(sf12mcs_dv_t1-mean(sf12mcs_dv_t1)))"
+df_y_str$method[df_y_str$Var1=="age_pcs"] <- "~I(age_bin*sf12pcs_dv_t1-mean(sf12pcs_dv_t1, na.rm = TRUE)))"
+df_y_str$method[df_y_str$Var1=="age_mcs"] <- "~I(age_bin*sf12mcs_dv_t1-mean(sf12pcs_dv_t1, na.rm = TRUE)))"
+df_y_str$method[df_y_str$Var1=="age_srh"] <- "~I(age_bin*srh_bin2)"
+df_y_str$method[df_y_str$Var1=="age_ghq"] <- "~I(age_bin*ghq_bin)"
+df_y_str$method[df_y_str$Var1=="rel_pov_pcs"] <- "~I(rel_pov_t0*sf12pcs_dv_t1-mean(sf12pcs_dv_t1, na.rm = TRUE)))"
+df_y_str$method[df_y_str$Var1=="rel_pov_mcs"] <- "~I(rel_pov_t0*sf12mcs_dv_t1-mean(sf12pcs_dv_t1, na.rm = TRUE)))"
 df_y_str$method[df_y_str$Var1=="rel_pov_srh"] <- "~I(rel_pov_t0*srh_bin2)"
 df_y_str$method[df_y_str$Var1=="rel_pov_ghq"] <- "~I(rel_pov_t0*ghq_bin)"
 
