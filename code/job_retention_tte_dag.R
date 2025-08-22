046f1626714079c534e0f32d327ba29a1f24c436
Andrew Pulford <andrew.pulford@phs.scot>
2025-08-22 15:56:36 +0100

    had accidently over-written with strobe flowchart code - correct code restored

--- code/job_retention_tte_dag.R
+++ code/job_retention_tte_dag.R
@@ -10,65 +10,156 @@ options(scipen=999)
 
 ## load packages
 library(DiagrammeR)
+library(tidyverse)
 library(DiagrammeRsvg)
 library(magrittr)
 library(rsvg)
+library(xml2)
+
 
 
 ################################################################################
-##### create DAG #####
+#####                              create DAG                              #####
 ################################################################################
 
-strobe <- grViz("
-digraph {
-  graph [ranksep = 0.2]
 
-  node [shape = box, width = 3, fontname = arial, fontsize = 12]
-    F [label = '29,456 lost to follow-up:\\n
-    3,584 self-employed or undertaking\\nunpaid work for family business at t1\\n
-    134	government training scheme or\\napprenticeship at t1\\n
-    1,316	full-time student at t1\\n
-    1,936	retired at t1\\n
-    1,328	on maternity leave at t1\\n
-    974	caring for family or home at t1\\n
-    518	long-term sick or disabled at t1\\n
-    279	other unspecified employment\\nstatus at t1\\n
-    19,387	health outcomes not measured\\nat t0 or t1
-    ']
-    A [label = '300,409 potential person-trials screened for eligibility']
-    B [label = '81,741 potential person-trials not eligible for trial:\\n
-    71,960 not working-age\\n
-    91,522 not employee at t0']
-    C [label = '136,927 eligible potential person-trials']
-    D [label = '134,243 allocated to treatment group']
-    E [label = '2,684 allocated to control group']
-    
-    G [label = '641 lost to follow-up:\\n
-    5	retired at t1\\n
-    636	health outcomes not measured at t0 or t1']
-    H[label = '104,787 potential-person trial outcomes assessed']
-    I[label = '2,043 potential-person trial outcomes assessed']
+#### Mental health outcomes ----------------------------------------------------
 
-  edge [minlen = 2]
-    A->B
-    A->C
-    C->D
-    C->E
-    F->D [dir=back]
-    E->G
-    D->H
-    E->I
-  { rank = same; A; B }
-  { rank = same; C }
-  { rank = same; F; D; E; G}
-  { rank = same; H; I }
-}
-")
+DAGJRS <-DiagrammeR::grViz("
+                  digraph{
+                  graph[ranksep=0.2]
+                  
+                  node[shape=plaintext,fontname=Arial]
+                  
+                    Inv[label ='Time invariant confounders',shape=oval]
+                    Var0[label='Time-varying \nconfounders T0',shape=box]
+                    Var1[label='Time-varying \nconfounders T1',shape=box]
+                    Age0[label='Age T0', shape=oval]
+                    Age1[label='Age T1', shape=oval]
+                    Emp0[label='Employment status \nat T0']
+                    JRS[label=<<B>Job retention scheme</B>>,fontcolor=darkgreen]
+                    MH0[label='Mental Health T0']
+                    MH1[label=<<B>Mental Health T1</B>>,fontcolor=darkorange2]
 
-strobe
+                  edge[minlen=2]
+                    Inv->Var0->MH1
+                    Inv->Var1
+                    Inv->MH0->MH1
+                    Inv->MH1
+                    Inv->Emp0
+                    Inv->JRS
+                    Var0->Var1
+                    Var0->MH1
+                    Var0->Emp0
+                    Var1->JRS
+                    MH0->Var0
+                    Var1->MH1
+                    Emp0->MH1
+                    Emp0->JRS
+                    MH0->Emp0
+                    JRS->MH1
+                    Age0->Emp0
+                    Age0->MH0
+                    Age0->Var0
+                    Age1->JRS
+                    Age1->MH1
+                    Age1->Var1
+                    Emp0->Var1
+                    
+                    
+                    
+                  {rank=min; Inv}
+                  {rank=same; Var0; Var1; Age0; Age1}
+                  {rank=same; Emp0; JRS}
+                  {rank=max; MH0; MH1}
+                  }
+                  ")
 
+DAGJRS
 
-strobe %>% 
+DAGJRS %>% 
   export_svg() %>% 
   charToRaw() %>% 
-  rsvg_png("./output/prec_emp_strobe_flow.png")
+  rsvg_png("./output/job_retention_tte_dag_mh.png")
+
+#### Alt version ---------------------------------------------------------------
+
+DAGJRS2 <-DiagrammeR::grViz("
+                  digraph{
+                  graph[ranksep=0.2]
+                  
+                  node[shape=plaintext,fontname=Arial]
+                  
+                    Inv[label ='Time invariant confounders',shape=oval]
+                    Var0[label='Time-varying \nconfounders T0',shape=box]
+                    Var1[label='Time-varying \nconfounders T1',shape=box]
+                    Age0[label='Age T0', shape=oval]
+                    Age1[label='Age T1', shape=oval]
+                    Emp0[label='Employment status \nat T0']
+                    JRS[label=<<B>Job retention scheme</B>>,fontcolor=darkgreen]
+                    MH0[label='Mental Health T0']
+                    MH1[label=<<B>Mental Health T1</B>>,fontcolor=darkorange2]
+
+                  edge[minlen=2]
+                    Inv->Var0->MH1
+                    Inv->Var1
+                    Inv->MH0->MH1
+                    Inv->MH1
+                    Inv->Emp0
+                    Var0->Var1
+                    Var0->MH1
+                    Var0->Emp0
+                    MH0->Var0
+                    Var1->MH1
+                    Emp0->MH1
+                    Emp0->JRS
+                    MH0->Emp0
+                    JRS->MH1
+                    Age0->Emp0
+                    Age0->MH0
+                    Age0->Var0
+                    Age1->MH1
+                    Age1->Var1
+                    Emp0->Var1
+                    
+                    
+                    
+                  {rank=min; Inv}
+                  {rank=same; Var0; Var1; Age0; Age1}
+                  {rank=same; Emp0; JRS}
+                  {rank=max; MH0; MH1}
+                  }
+                  ") 
+
+DAGJRS2
+
+DAGJRS2 %>%
+  export_svg() %>% 
+  charToRaw() %>% 
+  rsvg_png("./output/job_retention_tte_dag_mh_ALT.png")
+
+#### DAG key -------------------------------------------------------------------
+DAGJRS_key <- DiagrammeR::grViz("
+                  digraph{
+                  graph[ranksep=0]
+                  
+                  node[shape=plaintext,fontname=Arial]
+                  
+                  Key1[label='Time invariant confounders: baseline outcome measure, gender, ethnicity,\neducational attainment, industry classification,\noccupational classification, size of workplace, full or part-time working,\nusual working hours, disability or long-term condition, employment contract,\nemployment continuity since previous wave,\nmultiple employment and relative poverty (<60% of median income)',shape=box]
+                  Key2[label='Time-varying covariates: age, marital status, region and dependent children',shape=box]
+                  
+                  edge[minlen=2]
+                  Key1->Key2 [style=invis]
+                  
+                  {rank=max; Key2}
+                  }
+                  ")
+
+DAGJRS_key
+
+DAGJRS_key %>%
+  export_svg() %>% 
+  charToRaw() %>% 
+  rsvg_png("./output/job_retention_tte_dag_mh_key.png")
+
+
